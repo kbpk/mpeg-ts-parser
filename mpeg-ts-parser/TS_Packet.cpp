7dfc00e9c23d10f0b68065b97033f37b4dd2b4e9
kbpk <jakub.piasecki99@gmail.com>
2021-06-23 01:34:14 +0200

    Add ts packet constructor

--- mpeg-ts-parser/TS_Packet.cpp
+++ mpeg-ts-parser/TS_Packet.cpp
@@ -1,1 +1,13 @@
 ﻿#include "TS_Packet.h"
+
+#include <cstring>
+
+namespace ts
+{
+  Packet::Packet(const uint8_t* buffer)
+  {
+    memcpy(this->buffer, buffer, LENGTH);
+
+    header = new PacketHeader{buffer};
+  }
+}
