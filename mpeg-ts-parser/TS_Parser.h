1851dc8ab9ad3a4cc5147fa9cef0546331b046cd
kbpk <jakub.piasecki99@gmail.com>
2021-06-23 01:21:19 +0200

    Add ts parser definition

---
+++ mpeg-ts-parser/TS_Parser.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <cstdint>
+#include <fstream>
+#include <string>
+
+#include "TS_Packet.h"
+
+namespace ts
+{
+  class Parser
+  {
+    std::ifstream ts_file;
+    uint8_t* buffer;
+  public:
+    explicit Parser(const std::string& ts_filename);
+
+    ~Parser()
+    {
+      delete[] buffer;
+    }
+
+    [[nodiscard]] bool has_maybe_next_packet() { return ts_file.peek() != std::ifstream::traits_type::eof(); }
+    [[nodiscard]] Packet* next_packet();
+  };
+}
