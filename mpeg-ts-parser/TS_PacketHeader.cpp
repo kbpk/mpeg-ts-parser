b632bef9921c3af5e43ef3d0d851c49663dc5a4c
kbpk <jakub.piasecki99@gmail.com>
2021-06-23 01:07:46 +0200

    Add ts packet header definition

---
+++ mpeg-ts-parser/TS_PacketHeader.cpp
@@ -0,0 +1,1 @@
+﻿#include "TS_PacketHeader.h"
---
+++ mpeg-ts-parser/TS_PacketHeader.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <cstdint>
+
+namespace ts
+{
+  class PacketHeader
+  {
+  public:
+    enum PacketIdentifier : uint16_t
+    {
+      PAT = 0x0000,
+      CAT = 0x0001,
+      TSDT = 0x0002,
+      IPMT = 0x0003,
+      // DVB specific PID
+      NIT = 0x0010,
+      // DVB specific PID
+      SDT = 0x0011,
+      NuLL = 0x1FFF
+    };
+
+    static constexpr size_t LENGTH = 4;
+    static constexpr uint8_t SYNC_BYTE_CORRECT = 0x47;
+
+  private:
+    uint8_t sync_byte;
+    bool transport_error_indicator;
+    bool payload_unit_start_indicator;
+    uint8_t transport_priority_ : 1;
+    uint16_t packet_identifier : 13;
+    uint8_t transport_scrambling_control : 2;
+    uint8_t adaptation_field_control : 2;
+    uint8_t continuity_counter : 4;
+
+  public:
+    explicit PacketHeader(const uint8_t* buffer);
+
+    [[nodiscard]] uint16_t get_packet_identifier() const { return packet_identifier; }
+    [[nodiscard]] bool get_payload_unit_start_indicator() const { return payload_unit_start_indicator; }
+    [[nodiscard]] uint8_t get_continuity_counter() const { return continuity_counter; }
+  };
+}
