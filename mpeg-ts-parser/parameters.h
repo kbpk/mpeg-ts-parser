e5a6ca2094c7516d5deeed117b351b0c79241da4
kbpk <jakub.piasecki99@gmail.com>
2021-06-23 01:00:14 +0200

    Add system parameters

---
+++ mpeg-ts-parser/parameters.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+static constexpr unsigned int BASE_CLOCK_FREQUENCY_kHz = 90;
+static constexpr unsigned int BASE_CLOCK_FREQUENCY_Hz = 90'000;
+static constexpr unsigned int EXTERNAL_CLOCK_FREQUENCY_kHz = 27'000;
+static constexpr unsigned int EXTERNAL_CLOCK_FREQUENCY_Hz = 27'000'000;
+static constexpr unsigned int BASE_TO_EXTERNAL_CLOCK_MULTIPLIER = 300;
