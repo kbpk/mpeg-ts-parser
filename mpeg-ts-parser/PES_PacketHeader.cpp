c1967b61b671004ed8a8062bb8de54384eaa056f
kbpk <jakub.piasecki99@gmail.com>
2021-06-23 20:22:31 +0200

    Add pes packet header definition

---
+++ mpeg-ts-parser/PES_PacketHeader.cpp
@@ -0,0 +1,1 @@
+﻿#include "PES_PacketHeader.h"
---
+++ mpeg-ts-parser/PES_PacketHeader.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+#include <cstdint>
+#include <vector>
+
+namespace pes
+{
+  class PacketHeader
+  {
+  public:
+    struct Timestamp
+    {
+      static constexpr size_t LENGTH = 5;
+
+      uint64_t value : 33 = 0;
+
+      [[nodiscard]] double seconds() const;
+
+      [[nodiscard]] static Timestamp* parse(const std::vector<uint8_t>& buffer, int index);
+    };
+
+    struct GuaranteedFields
+    {
+      static constexpr size_t LENGTH = 6;
+
+      uint32_t packet_start_code_prefix : 24;
+      uint8_t stream_id;
+      uint16_t packet_length;
+    };
+
+    struct ExtraFields
+    {
+      struct OptionalExtraFields
+      {
+        static constexpr size_t PTS_START_IDX = (GuaranteedFields::LENGTH + 4) - 1;
+        static constexpr size_t PTS_LENGTH_NEEDED = GuaranteedFields::LENGTH + 3 + Timestamp::LENGTH;
+        static constexpr size_t DTS_START_IDX = (GuaranteedFields::LENGTH + 4) - 1 + Timestamp::LENGTH;
+        static constexpr size_t DTS_LENGTH_NEEDED = GuaranteedFields::LENGTH + 3 + 2 * Timestamp::LENGTH;
+
+        Timestamp* presentation_timestamp = nullptr;
+        Timestamp* decoding_timestamp = nullptr;
+
+        ~OptionalExtraFields()
+        {
+          delete presentation_timestamp;
+          delete decoding_timestamp;
+        }
+
+        [[nodiscard]] static Timestamp* parse_presentation_timestamp(const std::vector<uint8_t>& buffer);
+        [[nodiscard]] static Timestamp* parse_decoding_timestamp(const std::vector<uint8_t>& buffer);
+      };
+
+      static constexpr size_t GUARANTEED_LENGTH = 3;
+      static constexpr size_t TIMESTAMP_FLAGS_LENGTH_NEEDED = GuaranteedFields::LENGTH + GUARANTEED_LENGTH - 1;
+      static constexpr size_t HDL_LENGTH_NEEDED = GuaranteedFields::LENGTH + GUARANTEED_LENGTH;
+
+      uint8_t timestamp_flags : 2;
+      uint8_t header_data_length;
+      OptionalExtraFields* optional_extra_fields = nullptr;
+
+      ~ExtraFields() { delete optional_extra_fields; }
+
+      [[nodiscard]] size_t length() const;
+
+      [[nodiscard]] static uint8_t parse_timestamp_flags(const std::vector<uint8_t>& buffer);
+      [[nodiscard]] static uint8_t parse_header_data_length(const std::vector<uint8_t>& buffer);
+    };
+
+    enum StreamId : uint8_t
+    {
+      program_stream_map = 0xBC,
+      padding_stream = 0xBE,
+      private_stream_2 = 0xBF,
+      ECM = 0xF0,
+      EMM = 0xF1,
+      program_stream_directory = 0xFF,
+      DSMCC_stream = 0xF2,
+      ITUT_H222_1_type_E = 0xF8,
+    };
+
+  private:
+    GuaranteedFields* guaranteed_fields = nullptr;
+    ExtraFields* extra_fields = nullptr;
+
+    const std::vector<uint8_t>& buffer;
+
+  public:
+    explicit PacketHeader(const std::vector<uint8_t>& buffer);
+
+    ~PacketHeader()
+    {
+      delete guaranteed_fields;
+      delete extra_fields;
+    }
+
+    void init_guaranteed_fields();
+    void init_extra_fields();
+    void init_optional_extra_fields() const;
+
+    [[nodiscard]] bool can_have_extra_fields() const;
+    [[nodiscard]] bool can_have_optional_extra_fields() const;
+
+    [[nodiscard]] bool has_guaranteed_fields() const { return guaranteed_fields; }
+    [[nodiscard]] bool has_extra_fields() const { return extra_fields; }
+    [[nodiscard]] bool has_optional_extra_fields() const { return extra_fields && extra_fields->optional_extra_fields; }
+  };
+}
