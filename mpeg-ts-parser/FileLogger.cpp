fc5e057df15ec58fa27f4ae2471a3f382f74c6f1
kbpk <jakub.piasecki99@gmail.com>
2021-06-24 14:07:08 +0200

    Add file logger

---
+++ mpeg-ts-parser/FileLogger.cpp
@@ -0,0 +1,13 @@
+﻿#include "FileLogger.h"
+
+FileLogger::FileLogger(const std::string& log_filename)
+{
+  log_file.open(log_filename);
+  if (!log_file.is_open())
+    throw std::exception("Couldn't open log file");
+}
+
+void FileLogger::log_line(const std::string& line)
+{
+  log_file << line << std::endl;
+}
---
+++ mpeg-ts-parser/FileLogger.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <fstream>
+#include <ostream>
+#include <string>
+
+class FileLogger
+{
+  std::ofstream log_file;
+
+public:
+  explicit FileLogger(const std::string& log_filename);
+
+  void log_line(const std::string& line);
+};
