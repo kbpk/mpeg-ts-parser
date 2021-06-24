80a66f1496cc790d31a742cbaff98443cc9ac2d7
kbpk <jakub.piasecki99@gmail.com>
2021-06-24 14:03:32 +0200

    Add to string & buffers methods to pes packet

--- mpeg-ts-parser/PES_Packet.cpp
+++ mpeg-ts-parser/PES_Packet.cpp
@@ -1,9 +1,11 @@
 ﻿#include "PES_Packet.h"
 
+#include <iomanip>
+#include <sstream>
+
 namespace pes
 {
-  void Packet::absorb_ts_packet(const int& idx,
-                                const ts::Packet* ts_packet)
+  void Packet::absorb_ts_packet(const int& idx, const ts::Packet* ts_packet)
   {
     ts_packet_buffer.emplace_back(idx, new ts::Packet{ts_packet});
 
@@ -12,5 +14,66 @@ namespace pes
 
     for (auto i = ts::Packet::LENGTH - payload_length; i < ts::Packet::LENGTH; ++i)
       buffer.push_back(ts_buffer[i]);
+  }
+
+  std::vector<uint8_t> Packet::get_data_buffer() const
+  {
+    std::vector<uint8_t> data_buffer;
+
+    const auto& buffer_size = buffer.size();
+    const auto data_length = buffer_size - header->get_length();
+
+    for (auto i = buffer_size - data_length; i < buffer_size; ++i)
+      data_buffer.push_back(buffer[i]);
+
+    return data_buffer;
+  }
+
+  std::string Packet::summary_to_string() const
+  {
+    std::stringstream ss;
+
+    const auto& buffer_size = buffer.size();
+    const auto& header_length = header->get_length();
+    const auto data_length = buffer_size - header_length;
+
+    ss
+      << "PES: PcktLen=" << buffer_size
+      << " HeadLen=" << header_length
+      << " DataLen=" << data_length;
+
+    return ss.str();
+  }
+
+  std::string Packet::ts_packet_buffer_to_string() const
+  {
+    std::stringstream ss;
+
+    const auto& ts_packet_buffer_size = ts_packet_buffer.size();
+
+    for (auto i = 0; i < ts_packet_buffer_size; ++i)
+    {
+      const auto& [idx, ts_packet] = ts_packet_buffer[i];
+
+      ss << std::setw(10) << std::setfill('0') << idx << " " << ts_packet->to_string();
+
+      if (i == 0)
+      {
+        ss << " Assembling Started " << header->to_string();
+        if (ts_packet_buffer_size != 1)
+          ss << std::endl;
+      }
+      if ((i != 0 && i == ts_packet_buffer_size - 1) || (i == 0 && ts_packet_buffer_size == 1))
+      {
+        ss << " Assembling Finished " << summary_to_string();
+        break;
+      }
+      if (i != 0)
+      {
+        ss << " Assembling Continue " << std::endl;
+      }
+    }
+
+    return ss.str();
   }
 }
--- mpeg-ts-parser/PES_Packet.h
+++ mpeg-ts-parser/PES_Packet.h
@@ -16,7 +16,7 @@ namespace pes
   public:
     ~Packet()
     {
-      for (auto&& [i, ts_packet] : ts_packet_buffer)
+      for (auto& [i, ts_packet] : ts_packet_buffer)
         delete ts_packet;
 
       delete header;
@@ -25,5 +25,10 @@ namespace pes
     void absorb_ts_packet(const int& idx, const ts::Packet* ts_packet);
 
     [[nodiscard]] PacketHeader* get_header() { return header; }
+    [[nodiscard]] size_t get_buffer_size() const { return buffer.size(); }
+    [[nodiscard]] std::vector<uint8_t> get_data_buffer() const;
+
+    [[nodiscard]] std::string summary_to_string() const;
+    [[nodiscard]] std::string ts_packet_buffer_to_string() const;
   };
 }
