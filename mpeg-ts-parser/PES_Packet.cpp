255eca309c51ad324ad115f87d161f5ae2ab9041
kbpk <jakub.piasecki99@gmail.com>
2021-06-24 02:18:00 +0200

    Add pes packet definition

---
+++ mpeg-ts-parser/PES_Packet.cpp
@@ -0,0 +1,16 @@
+﻿#include "PES_Packet.h"
+
+namespace pes
+{
+  void Packet::absorb_ts_packet(const int& idx,
+                                const ts::Packet* ts_packet)
+  {
+    ts_packet_buffer.emplace_back(idx, new ts::Packet{ts_packet});
+
+    const auto& payload_length = ts_packet->get_payload_length();
+    const auto* ts_buffer = ts_packet->get_buffer();
+
+    for (auto i = ts::Packet::LENGTH - payload_length; i < ts::Packet::LENGTH; ++i)
+      buffer.push_back(ts_buffer[i]);
+  }
+}
---
+++ mpeg-ts-parser/PES_Packet.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <cstdint>
+#include <vector>
+
+#include "PES_PacketHeader.h"
+#include "TS_Packet.h"
+
+namespace pes
+{
+  class Packet
+  {
+    std::vector<uint8_t> buffer;
+    std::vector<std::pair<int, ts::Packet*>> ts_packet_buffer;
+    PacketHeader* header = new PacketHeader{buffer};
+
+  public:
+    ~Packet()
+    {
+      for (auto&& [i, ts_packet] : ts_packet_buffer)
+        delete ts_packet;
+
+      delete header;
+    }
+
+    void absorb_ts_packet(const int& idx, const ts::Packet* ts_packet);
+
+    [[nodiscard]] PacketHeader* get_header() { return header; }
+  };
+}
